6f724bd6dd81f6e27d3fdf6fe7a4e187486fbc6c
yatm <yathishyadav.km@gmail.com>
2020-02-15 13:28:18 +0530

    Backtracking solution to find the shortest path in a matrix

--- ShortestPathInBinaryMatrix.java
+++ ShortestPathInBinaryMatrix.java
@@ -1,3 +1,32 @@
+/**
+ * Shortest Path in Binary Matrix
+ *
+ * In an N by N square grid, each cell is either empty (0) or blocked (1).
+ *
+ * A clear path from top-left to bottom-right has length k if and only if it is composed of cells C_1, C_2, ..., C_k such that:
+ *
+ * Adjacent cells C_i and C_{i+1} are connected 8-directionally (ie., they are different and share an edge or corner)
+ * C_1 is at location (0, 0) (ie. has value grid[0][0])
+ * C_k is at location (N-1, N-1) (ie. has value grid[N-1][N-1])
+ * If C_i is located at (r, c), then grid[r][c] is empty (ie. grid[r][c] == 0).
+ * Return the length of the shortest such clear path from top-left to bottom-right.  If such a path does not exist, return -1.
+ *
+ * Example 1:
+ *
+ * Input: [[0,1],[1,0]]
+ *
+ *
+ * Output: 2
+ *
+ *
+ *
+ * Example 2:
+ *
+ * Input: [[0,0,0],[1,1,0],[1,1,0]]
+ *
+ *
+ * Output: 4
+ */
 public class ShortestPathInBinaryMatrix {
 
     private static class Result {
@@ -27,12 +56,18 @@ public class ShortestPathInBinaryMatrix {
         return (result.minShortestPath == Integer.MAX_VALUE) ? -1 : result.minShortestPath;
     }
 
+    /**
+     * Backtrack and explore all the eight adjacent cells of a current cell to find the shortest path from top left to bottom right in a matrix
+     * @param grid
+     * @param rowIndex
+     * @param colIndex
+     * @param markVisitedCell
+     * @param maxPathSoFar
+     */
     private void shortestPathBinaryMatrix(int[][] grid, int rowIndex, int colIndex, boolean[][] markVisitedCell, int maxPathSoFar){
-
         if(rowIndex == grid.length -1 && colIndex == grid[0].length -1 && grid[rowIndex][colIndex] == 0){
             result.minShortestPath = Math.min(maxPathSoFar , result.minShortestPath);
         }
-
         markVisitedCell[rowIndex][colIndex] = Boolean.TRUE;
         // backtrack top left
         if(rowIndex-1 >=0 && colIndex-1 >= 0 && !markVisitedCell[rowIndex -1][colIndex -1] && grid[rowIndex - 1][colIndex - 1] == 0 ){
