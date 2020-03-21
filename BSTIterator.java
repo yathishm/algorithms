9184ac9f162b586529722868e53b5bf00951a211
yatm <yathishyadav.km@gmail.com>
2020-03-21 14:18:41 +0530

    adding solution to Binary Search Tree iterator

---
+++ BSTIterator.java
@@ -0,0 +1,87 @@
+import java.util.Stack;
+
+/**
+ * Binary Search Tree Iterator
+ * Implement an iterator over a binary search tree (BST). Your iterator will be initialized with the root node of a BST.
+ *
+ * Calling next() will return the next smallest number in the BST.
+ *
+ *
+ *
+ * Example:
+ * BSTIterator iterator = new BSTIterator(root);
+ * iterator.next();    // return 3
+ * iterator.next();    // return 7
+ * iterator.hasNext(); // return true
+ * iterator.next();    // return 9
+ * iterator.hasNext(); // return true
+ * iterator.next();    // return 15
+ * iterator.hasNext(); // return true
+ * iterator.next();    // return 20
+ * iterator.hasNext(); // return false
+ *
+ *
+ * Note:
+ *
+ * next() and hasNext() should run in average O(1) time and uses O(h) memory, where h is the height of the tree.
+ * You may assume that next() call will always be valid, that is, there will be at least a next smallest number in the BST when next() is called.
+ */
+public class BSTIterator {
+
+    private TreeNode root;
+    private Stack<TreeNode> stackOfNodes;
+
+    public BSTIterator(TreeNode root) {
+        this.root = root;
+        stackOfNodes = new Stack<>();
+        initializeToFirstSmallestNode(root);
+    }
+
+    /*
+    * @return the next smallest number
+    */
+    public int next() {
+        TreeNode retNode = stackOfNodes.pop();
+        if(null != retNode.right){
+            initializeToFirstSmallestNode(retNode.right);
+        }
+        return retNode.val;
+    }
+
+    /*
+    * @return whether we have a next smallest number
+    *
+    */
+    public boolean hasNext() {
+        return !stackOfNodes.isEmpty();
+    }
+
+    /**
+     * Intialize's the top of the stack with the next smallest Node in BST.
+     * @param root
+     */
+    private void initializeToFirstSmallestNode(TreeNode root){
+        TreeNode temp = root;
+        while(null != temp){
+            stackOfNodes.push(temp);
+            temp = temp.left;
+        }
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
